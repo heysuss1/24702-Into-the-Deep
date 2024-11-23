0ce4c7a8206fe6d79f473b8f6274612a0c4523f6
Niko-mp4 <107970954+DestinVo@users.noreply.github.com>
2024-11-23 14:28:10 -0500

    FEAT: Created hanging specimen auto!
    Used state machines and two separate recursive switch statement functions to use all 6 motors at once!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAuto.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
+import android.drm.DrmStore;
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
@@ -23,8 +25,14 @@ public class TestAuto extends OpMode {
         HANG_SPECIMEN
 
     }
+    enum ActionState{
+        RAISE_ARMS,
+        HANG_SPECIMEN
+
+    }
 
     State state = State.GO_TO_BAR;
+    ActionState actionState = ActionState.RAISE_ARMS;
 //    PathBuilder toSubmersible;
 
     //Assumes robot starts at (5, 65)
@@ -32,14 +40,15 @@ public class TestAuto extends OpMode {
         follower = new Follower(hardwareMap);
         robot.init(hardwareMap);
         follower.setStartingPose(starting);
-
+        robot.armVertical.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.armVertical.setTargetPosition(1200);
+        robot.armVertical.setPower(0.8);
         follower.followPath(path());
-
     }
 
     public static PathChain path(){
         PathBuilder builder = new PathBuilder();
-        builder.addPath((new BezierLine(new Point(5, 65, Point.CARTESIAN), new Point(31, 65, Point.CARTESIAN))))
+        builder.addPath((new BezierLine(new Point(5, 65, Point.CARTESIAN), new Point(31.6, 65, Point.CARTESIAN))))
                 .setTangentHeadingInterpolation();
 //                .addPath(new BezierLine(new Point(40, 65, Point.CARTESIAN), new Point(5, 65, Point.CARTESIAN)))
 //                .setTangentHeadingInterpolation();
@@ -48,31 +57,71 @@ public class TestAuto extends OpMode {
     }
 
 
-
-    public void doAuto(){
+    public void setPathState(State prevState){
+        state = prevState;
+        autonomousPathUpdate();
+    }
+    public void autonomousPathUpdate(){
         switch (state){
             case GO_TO_BAR:
-
+                follower.followPath(path());
+                setPathState(State.HANG_SPECIMEN);
                 break;
             default:
                 stop();
 
 
+        }
+    }
 
+    public void setAction(ActionState prevState){
+        actionState = prevState;
+        autonomousActionUpdate();
+    }
+    public void autonomousActionUpdate(){
+        switch (actionState){
+            case RAISE_ARMS:
+                robot.leftServo.setPosition(.69);
+                robot.rightServo.setPosition(.1);
+                robot.armExtension.setPower(1);
+                robot.armExtension.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                robot.armExtension.setTargetPosition(-1200);
+                robot.armVertical.setPower(1);
+                robot.armVertical.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                robot.armVertical.setTargetPosition(2300);
+                if (robot.armExtension.getCurrentPosition() < -1199 && robot.armVertical.getCurrentPosition() > 2290) {
+                    setAction(ActionState.HANG_SPECIMEN);
+                }
+                break;
+            case HANG_SPECIMEN:
+                if (follower.getCurrentPath().isAtParametricEnd()){
+                    robot.armExtension.setPower(1);
+                    robot.armExtension.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    robot.armExtension.setTargetPosition(-350);
+                    robot.armVertical.setPower(1);
+                    robot.armVertical.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    robot.armVertical.setTargetPosition(2100);
+                }
+
+//                if (robot.armExtension.getCurrentPosition() < - && robot.armVertical.getCurrentPosition() > ) {
+//                    setAction(ActionState.HANG_SPECIMEN);
+//                }
+                break;
+            default:
+                stop();
 
         }
     }
+
+//    public void
+
     @Override
     public void loop(){
         telemetry.addData("X Position", follower.getPose());
         telemetry.addData("Position", robot.armExtension.getCurrentPosition());
-        robot.armExtension.setPower(0.8);
-        robot.armExtension.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        robot.armExtension.setTargetPosition(-1200);
-        robot.armExtension.setPower(0.8);
-        if (robot.armExtension.getCurrentPosition() < 1199) {
-            robot.armExtension.setPower(0);
-        }
+        telemetry.addData("Arm Position", robot.armVertical.getCurrentPosition());
+        autonomousPathUpdate();
+        autonomousActionUpdate();
         follower.update();
 
 
