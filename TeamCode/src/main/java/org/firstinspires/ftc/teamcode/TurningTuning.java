1603f2dc3643b19d0371f8bdcaf626668c460e4b
Amelia2009 <acleopanda@gmail.com>
2024-12-30 15:20:30 -0500

    added p controller for turning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TurningTuning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TurningTuning.java
@@ -3,8 +3,10 @@ package org.firstinspires.ftc.teamcode;
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.arcrobotics.ftclib.controller.PController;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.pedroPathing.follower.Follower;
@@ -16,13 +18,25 @@ public class TurningTuning extends OpMode {
     private Telemetry telemetryA;
     public static double kP;
     Follower follower;
+    PController controller;
     double output;
     public static int target;
     FtcDashboard dashboard = FtcDashboard.getInstance();
     SquidPID squid;
     public void init(){
+        robot.init(hardwareMap);
         telemetryA = new MultipleTelemetry(this.telemetry, dashboard.getTelemetry());
+        controller = new PController(kP);
+        follower = new Follower(hardwareMap);
     }
     public void loop(){
+        output = controller.calculate(follower.getPose().getHeading(), target);
+        if (follower.getPose().getHeading() < 180 ){
+            robot.setPower(-output,-output, output, output);
+        } else {
+            robot.setPower(output, output, -output, -output);
+        }
+
     }
+
 }
